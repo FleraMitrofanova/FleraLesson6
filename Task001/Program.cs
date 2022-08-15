f862bb3a651c950a2849e5d609ea28e265f146ae
Flera <Izzi_113@mail.ru>
2022-08-15 19:40:15 +0400

    Lesson6

---
+++ Task001/Program.cs
@@ -0,0 +1,21 @@
+﻿ //Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+ int Sum(int size)
+ {
+    int a;
+    int count = 0;
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write("Введите число: ");
+        a=Convert.ToInt32(Console.ReadLine());
+        if(a>0)
+        {
+            count++;
+        }
+
+    }
+    return count;
+ }
+ int size = 5;
+ int result = Sum(size);
+ Console.WriteLine(result);
---
+++ Task002/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+void Corner(int k1, int k2, int b1, int b2)
+{
+    int x, y = 0;
+    x = (b2-b1)/(k1-k2);
+    y = k2 * x + b2;
+
+
+    Console.WriteLine("Точка пересечения" + " x " +  x + " y " + y);
+}
+
+int k1,k2,b1,b2;
+Console.WriteLine("Введите координаты");
+k1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты");
+k2=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты");
+b1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты");
+b2=Convert.ToInt32(Console.ReadLine());
+
+Corner(k1,k2,b1,b2);
